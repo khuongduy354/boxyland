9025c6a4a481cbf24dd805c94c432da98573cd9b
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2023-01-14 16:18:15 +0700

    feat:growing plats

--- Enemy/EnemyBase.gd
+++ Enemy/EnemyBase.gd
@@ -3,18 +3,32 @@ extends KinematicBody2D
 export var spawn_chance = 10
 export var grow_time = 3
 export var phases = 2 
+export var current_phase = 1
+var mature = false
 export var speed = 1
 export var damage = 30 
 
 func _ready():
+	$AnimationPlayer.play(String(current_phase))
 	$Hitbox.damage = damage
 	add_to_group("enemy")
+	$Grow.wait_time = grow_time
 	pass 
 
 func _physics_process(delta):
 	pass
+	
+func grow(): 
+	current_phase+=1
+	if current_phase >= phases: 
+		mature = true
+		$Grow.disconnect("timeout",self,"grow")
+	$AnimationPlayer.play(String(current_phase))
+	pass
 
 func move(): 
+	if not mature: 
+		return
 	var s_rot = round(rotation_degrees)
 	var shoot_dir =  Vector2(0,-1).rotated(deg2rad(s_rot))
 	position += shoot_dir*speed
